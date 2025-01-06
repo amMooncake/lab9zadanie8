3449eab9ee1893f530bec100db70f98d54227fbe
alekm <alekmalawski123@gmail.com>
2025-01-06 16:44:33 +0100

    auth

--- Webapp/Controllers/OlympicsController.cs
+++ Webapp/Controllers/OlympicsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using Webapp.Models.Olympics;
 
 namespace Webapp.Controllers
 {
+    [Authorize(Roles = "amdin,user")]
     public class OlympicsController : Controller
     {
         private readonly OlympicsContext _context;
